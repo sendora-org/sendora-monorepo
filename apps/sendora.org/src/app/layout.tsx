2ef02685ae6c1a7aa6071841e462732f129addc5
Gary Shay <garyshay@proton.me>
2025-03-27 14:18:11 +0800

    Update html head

--- apps/sendora.org/src/app/layout.tsx
+++ apps/sendora.org/src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="icon"
           type="image/png"
@@ -40,8 +39,44 @@ export default function RootLayout({
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <meta name="apple-mobile-web-app-title" content="Sendora" />
+        <meta name="apple-mobile-web-app-title" content="SENDORA" />
         <link rel="manifest" href="/site.webmanifest" />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
+        />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="Author" content="Gary Shay" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="sendora.org" />
+        <meta property="og:url" content="https://sendora.org/native-coins/1" />
+        <meta property="og:site_name" content="SENDORA" />
+        <meta
+          property="og:image"
+          content="https://sendora.org/adswrapperads3.png"
+        />
+        <meta
+          property="og:description"
+          content="🚀 Building the best tool for Web3!"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="SENDORA" />
+        <meta name="twitter:title" content="sendora.org" />
+        <meta
+          name="twitter:description"
+          content="🚀 Building the best tool for Web3!"
+        />
+        <meta
+          name="twitter:image"
+          content="https://sendora.org/adswrapperads3.png"
+        />
+        <meta name="full-screen" content="yes" />
+        <meta name="browsermode" content="application" />
+        <meta name="x5-orientation" content="portrait" />
+        <meta name="x5-fullscreen" content="true" />
+        <meta name="x5-page-mode" content="app" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
