c1c2de85eb8deba00e059df6c0f6004901153cd0
Gary Shay <garyshay@proton.me>
2025-05-02 16:37:21 +0800

    Update MerkleMultiSend.sol

--- apps/sendora-contracts/src/MerkleMultiSend.sol
+++ apps/sendora-contracts/src/MerkleMultiSend.sol
@@ -5,127 +5,131 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleMultiSend {
-    mapping(bytes32 => bool) public executed;
+    address public constant feeCollector =
+        0x0c73894c8dBc2F48EFd57E340934D6A2c172fC81;
 
-    event MultisendERC20Executed(
-        address indexed referrer,
-        uint256 referralRewardETH,
-        address tokenAddress,
-        bytes32 taskId,
-        bytes32 merkleRoot,
-        uint256 totalRecipients
-    );
-
-    event MultisendETHExecuted(
-        address indexed referrer,
-        uint256 referralRewardETH,
-        bytes32 taskId,
-        bytes32 merkleRoot,
-        uint256 totalRecipients
-    );
-
-    address public feeCollector;
-    address public owner; // Contract owner address
-
-    constructor(address _feeCollector) {
-        feeCollector = _feeCollector;
+    struct Referrer {
+        address referrer;
+        bool isEligibleForCommission;
     }
 
-    // Function to send ETH to multiple recipients using a Merkle proof for validation
-    function multisendETH(
-        bytes32 taskId,
-        bytes32 merkleRoot,
+    struct Token {
+        address tokenAddress;
+        uint256 decimals;
+        string symbol;
+    }
+    struct Merkle {
+        bytes32 merkleRoot;
+        bytes32[] proof;
+    }
+
+    mapping(bytes32 => bool) public executed;
+
+    event Executed(
+        address indexed referrer,
+        address indexed from,
+        address indexed tokenAddress,
+        uint256 decimals,
+        string symbol,
+        uint256 amount,
+        uint256 toolFee,
+        uint256 timestamp
+    );
+
+    function execute(
+        Merkle calldata merkle,
+        bytes32 batchID,
+        Token calldata token,
+        Referrer calldata referrer,
         address[] calldata recipients,
-        uint256[] calldata amounts,
-        bytes32[] calldata proof,
-        address referrer
+        uint256[] calldata amounts
     ) external payable {
-        _validateMerkleOperation(
-            taskId,
-            merkleRoot,
-            proof,
-            recipients,
-            amounts
-        );
+        // validate
+        _validate(merkle, batchID, token, referrer, recipients, amounts);
 
         uint256 totalAmount = 0;
-        uint256 len = recipients.length;
+        uint256 toolFee = msg.value;
 
-        for (uint256 i = 0; i < len; i++) {
+        // send
+        if (token.tokenAddress == address(0)) {
+            toolFee = msg.value - totalAmount;
+            totalAmount = _sendETH(token, recipients, amounts);
+        } else {
+            totalAmount = _sendERC20(token, recipients, amounts);
+        }
+
+        // allocate fee
+        _allocateToolFee(referrer, toolFee);
+
+        emit Executed(
+            referrer.referrer,
+            msg.sender,
+            token.tokenAddress,
+            token.decimals,
+            token.symbol,
+            totalAmount,
+            toolFee,
+            block.timestamp
+        );
+    }
+
+    function _sendETH(
+        Token calldata token,
+        address[] calldata recipients,
+        uint256[] calldata amounts
+    ) internal returns (uint256) {
+        require(
+            token.tokenAddress == address(0),
+            "Invalid token, only ETH can be sent"
+        );
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < amounts.length; i++) {
             (bool success, ) = recipients[i].call{value: amounts[i]}("");
             require(success, "ETH transfer failed");
             totalAmount += amounts[i];
         }
-
-        uint256 fee = msg.value - totalAmount;
-        distributeFee(referrer, fee);
-
-        emit MultisendETHExecuted(referrer, fee / 2, taskId, merkleRoot, len);
+        return totalAmount;
     }
 
-    // Function to send ERC20 Token to multiple recipients using a Merkle proof for validation
-    function multisendERC20(
-        bytes32 taskId,
-        bytes32 merkleRoot,
+    function _sendERC20(
+        Token calldata token,
         address[] calldata recipients,
-        uint256[] calldata amounts,
-        bytes32[] calldata proof,
-        address referrer,
-        address tokenAddress
-    ) external payable {
-        _validateMerkleOperation(
-            taskId,
-            merkleRoot,
-            proof,
-            recipients,
-            amounts
+        uint256[] calldata amounts
+    ) internal returns (uint256) {
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < amounts.length; i++) {
+            _safeTransfer(token.tokenAddress, recipients[i], amounts[i]);
+            totalAmount += amounts[i];
+        }
+        return totalAmount;
+    }
+
+    function _validate(
+        Merkle calldata merkle,
+        bytes32 batchID,
+        Token calldata token,
+        Referrer calldata referrer,
+        address[] calldata recipients,
+        uint256[] calldata amounts
+    ) internal {
+        bytes32 executeID = keccak256(
+            abi.encode(batchID, merkle.merkleRoot, merkle.proof)
+        );
+        require(!executed[executeID], "Operation already executed");
+        require(recipients.length == amounts.length, "Input length  mismatch");
+        bytes32 leaf = keccak256(
+            abi.encode(batchID, token, referrer, recipients, amounts)
         );
 
-        uint256 len = recipients.length;
-        for (uint256 i = 0; i < len; i++) {
-            safeTransfer(tokenAddress, recipients[i], amounts[i]);
-        }
-
-        uint256 fee = msg.value;
-        distributeFee(referrer, fee);
-
-        emit MultisendERC20Executed(
-            referrer,
-            fee / 2,
-            tokenAddress,
-            taskId,
-            merkleRoot,
-            len
+        require(
+            MerkleProof.verify(merkle.proof, merkle.merkleRoot, leaf),
+            "Invalid Merkle proof"
         );
+
+        executed[executeID] = true;
     }
 
-    function distributeFee(address referrer, uint256 fee) internal {
-        uint256 referralRewardETH = fee / 2;
-
-        // Send referral reward if applicable
-        if (referrer != address(0)) {
-            (bool success, ) = payable(referrer).call{value: referralRewardETH}(
-                ""
-            );
-            require(success, "Referral reward transfer failed");
-        }
-
-        // Transfer remaining ETH to fee collector
-        (bool feeSuccess, ) = payable(feeCollector).call{
-            value: address(this).balance
-        }("");
-        require(feeSuccess, "Fee transfer failed");
-    }
-
-    function computeOperationId(
-        bytes32 taskId,
-        bytes32 merkleRoot,
-        bytes32[] calldata proof
-    ) external pure returns (bytes32) {
-        return keccak256(abi.encodePacked(taskId, merkleRoot, proof));
-    }
-
-    function safeTransfer(
+    function _safeTransfer(
         address _token,
         address _to,
         uint256 _amount
@@ -138,34 +142,30 @@ contract MerkleMultiSend {
                 _amount
             )
         );
-
         require(
             success && (data.length == 0 || abi.decode(data, (bool))),
             "Transfer failed"
         );
     }
 
-    function _validateMerkleOperation(
-        bytes32 taskId,
-        bytes32 merkleRoot,
-        bytes32[] calldata proof,
-        address[] calldata recipients,
-        uint256[] calldata amounts
+    function _allocateToolFee(
+        Referrer calldata referrer,
+        uint256 toolFee
     ) internal {
-        bytes32 operationId = keccak256(abi.encode(taskId, merkleRoot, proof));
-        require(!executed[operationId], "Operation already executed");
+        uint256 referralRewardETH = toolFee / 2;
+        if (
+            referrer.referrer != address(0) && referrer.isEligibleForCommission
+        ) {
+            (bool success, ) = payable(referrer.referrer).call{
+                value: referralRewardETH
+            }("");
+            require(success, "Referral reward transfer failed");
+        }
 
-        uint256 len = recipients.length;
-        require(len > 0, "No recipients");
-        require(len == amounts.length, "Input length  mismatch");
+        (bool feeSuccess, ) = payable(feeCollector).call{
+            value: address(this).balance
+        }("");
 
-        bytes32 leaf = keccak256(abi.encode(recipients, amounts));
-
-        require(
-            MerkleProof.verify(proof, merkleRoot, leaf),
-            "Invalid Merkle proof"
-        );
-
-        executed[operationId] = true;
+        require(feeSuccess, "ToolFee transfer failed");
     }
 }
