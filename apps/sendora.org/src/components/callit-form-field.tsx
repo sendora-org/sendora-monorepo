47df30b1eb21ef4ed3976b1ccc83fb89b8f4b3ae
Gary Shay <garyshay@proton.me>
2025-05-04 15:19:58 +0800

    Support filling in an array by pasting.

--- apps/sendora.org/src/components/callit-form-field.tsx
+++ apps/sendora.org/src/components/callit-form-field.tsx
@@ -1,5 +1,5 @@
 import { Disclosure } from '@headlessui/react';
-import { Input, Switch } from '@heroui/react';
+import { Button, Input, Switch } from '@heroui/react';
 import { Icon } from '@iconify/react';
 import type { ethers } from 'ethers';
 import type React from 'react';
@@ -12,7 +12,39 @@ type FormRendererProps = {
 };
 
 export const FormField: React.FC<FormRendererProps> = ({ param, name }) => {
-  const { control, register } = useFormContext();
+  const { control, register, setValue } = useFormContext();
+
+  // biome-ignore  lint/suspicious/noExplicitAny: reason
+  const paste = async (param: any, name: string, arrayLength: number) => {
+    console.log({ param, name, arrayLength });
+
+    const array = [];
+
+    param.replace([]);
+
+    try {
+      const text = await navigator.clipboard.readText();
+      const json = JSON.parse(text);
+      if (Array.isArray(json)) {
+        for (let i = 0; i < json.length; i++) {
+          if (i < arrayLength && arrayLength !== -1) {
+            array.push(json[i]);
+          }
+
+          if (arrayLength === -1) {
+            array.push(json[i]);
+          }
+        }
+      }
+
+      for (let i = 0; i < array.length; i++) {
+        param.remove(i);
+        param.insert(i, array[i]);
+      }
+    } catch (e) {
+      console.log('paste error', e);
+    }
+  };
 
   const renderPrimitive = () => {
     switch (param.type) {
@@ -85,14 +117,58 @@ export const FormField: React.FC<FormRendererProps> = ({ param, name }) => {
 
   if (param.baseType === 'array') {
     const arrayLength = param.arrayLength;
+
+    const fieldArray = useFieldArray({
+      control,
+      name,
+    });
+
+    console.log({ param }, 888);
     if (typeof arrayLength === 'number' && arrayLength >= 0) {
       return (
         <div className="p-2  border border-default-300 rounded">
           <div className="font-semibold block mb-2">
-            {param.name || name} (fixed array[{arrayLength}])
+            {param.name || name} (fixed array[{arrayLength}]){' '}
+            {param.type !== 'array' && param.type !== 'tuple' && (
+              <Button size='sm' onPress={() => paste(fieldArray, name, arrayLength)}>
+                paste
+              </Button>
+            )}
           </div>
+          {fieldArray.fields.map((field, index) => (
+            <div key={field.id} className="flex gap-2 items-center mb-2">
+              <div className="flex-1">
+                <FormField
+                  // biome-ignore lint/style/noNonNullAssertion: reason
+                  param={param.arrayChildren!}
+                  name={`${name}.${index}`}
+                />
+              </div>
+              <button
+                type="button"
+                onClick={() => fieldArray.remove(index)}
+                className="text-red-500 font-bold px-2 py-1 rounded border border-red-500 mt-0"
+              >
+                <Icon icon="mynaui:trash-solid" width="24" height="24" />
+              </button>
+            </div>
+          ))}
 
-          {Array.from({ length: arrayLength }).map((_, index) => (
+          {fieldArray.fields.length < arrayLength && (
+            <button
+              type="button"
+              onClick={() => {
+                if (fieldArray.fields.length < arrayLength) {
+                  fieldArray.append('');
+                }
+              }}
+              className="bg-blue-500 text-white px-2 py-1 rounded mt-2"
+            >
+              Add
+            </button>
+          )}
+
+          {/* {Array.from({ length: arrayLength }).map((_, index) => (
             <FormField
               // biome-ignore lint/suspicious/noArrayIndexKey: reason
               key={index}
@@ -100,19 +176,31 @@ export const FormField: React.FC<FormRendererProps> = ({ param, name }) => {
               param={param.arrayChildren!}
               name={`${name}.${index}`}
             />
-          ))}
+          ))} */}
+
+          {/* {fieldArray.fields.map((field, index) => (
+          <div key={field.id} className="flex gap-2 items-center mb-2">
+            <div className="flex-1">
+              <FormField
+                // biome-ignore lint/style/noNonNullAssertion: reason
+                param={param.arrayChildren!}
+                name={`${name}.${index}`}
+              />
+            </div>
+    
+          </div>
+        ))} */}
         </div>
       );
     }
 
-    const fieldArray = useFieldArray({
-      control,
-      name,
-    });
     return (
       <div className="p-2  border border-default-300 rounded my-2">
         <div className="font-semibold block mb-2">
-          {param.name || name} (array)
+          {param.name || name} (array){' '}
+          {param.type !== 'array' && param.type !== 'tuple' && (
+            <Button size='sm' onPress={() => paste(fieldArray, name, -1)}>paste</Button>
+          )}
         </div>
         {fieldArray.fields.map((field, index) => (
           <div key={field.id} className="flex gap-2 items-center mb-2">
